525722fdc8f6c9a1ccb996e777288e23b6a8a1af
songheony <songheony@gmail.com>
2024-09-27 11:41:59 +0900

    Add gcloud to Path

--- install_incl.sh
+++ install_incl.sh
@@ -1,3 +1,9 @@
+# Add gcloud to the path
+echo "source /google-cloud-sdk/path.zsh.inc" >> ${HOME}/.zshrc
+echo "source /google-cloud-sdk/completion.zsh.inc" >> ${HOME}/.zshrc
+echo "source /google-cloud-sdk/path.bash.inc" >> ${HOME}/.bashrc
+echo "source /google-cloud-sdk/completion.bash.inc" >> ${HOME}/.bashrc
+
 # Install INCL
 git clone https://github.com/lunit-io/incl-client.git --depth=1 /tmp/incl-client &&
     pip install /tmp/incl-client &&
